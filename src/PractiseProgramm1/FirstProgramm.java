b9594442098fff0d3f275f8de443fa5a0b966e45
Admin <jainmintu557@gmail.com>
2024-12-24 19:53:53 +0530

    firstfiles

--- src/PractiseProgramm1/FirstProgramm.java
+++ src/PractiseProgramm1/FirstProgramm.java
@@ -1,4 +1,8 @@
 package PractiseProgramm1;
 
-public class FirstProgramm {
+public class FirstProgramm
+{
+    public static void main(String[] args) {
+        System.out.println("welcome to java ");
+    }
 }
